355e8c5dd18834cd3193c98386b1036b0bf86d13
Deras Pangestu Gusti <deraspangestugusti@gmail.com>
2026-09-08 10:04:45 +0700

    Upload for Duties 1

---
+++ src/jobsheet3/Member.java
@@ -0,0 +1,43 @@
+package jobsheet3;
+
+public class Member 
+{
+    private String  nameCode,
+                    name;
+    private int limitLoan,
+                loanAmount;
+
+    public Member(String nameCode, String name, int limitLoan) {
+        this.nameCode = nameCode;
+        this.name = name;
+        this.limitLoan = limitLoan;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLimitLoan() {
+        return limitLoan;
+    }
+
+    public int getLoanAmount() {
+        return loanAmount;
+    }
+
+    public void borrow (int borrow)
+    {
+        if (borrow > limitLoan)
+            System.out.println("Maaf, jumlah peminjaman melebihi limit.");
+        else
+            loanAmount += borrow;
+    }
+
+    public void installment (int installment)
+    {
+        if (installment > loanAmount)
+            System.out.println("Maaf, jumlah cilcilan melebihi total peminjaman saat ini");
+        else
+            loanAmount -= installment;
+    }
+}
